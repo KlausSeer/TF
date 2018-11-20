a9a6f7c51f2ba4b90e3143077a2a801124228e1b
Duranator <43173906+Duranator@users.noreply.github.com>
2018-11-20 00:15:40 -0500

    Update Arbol_Escenario.h

--- TF_Enemigos/TF_Enemigos/Arbol_Escenario.h
+++ TF_Enemigos/TF_Enemigos/Arbol_Escenario.h
@@ -2,6 +2,9 @@
 
 #include "Escenario.h"
 
+using namespace System::IO;
+using namespace System::Xml;
+
 class Arbol_Escenario {
 
 	struct Node {
@@ -10,12 +13,13 @@ class Arbol_Escenario {
 		Node* left_Child_Esc;
 		Node* right_Child_Esc;
 		int profundidad;
-		
-		Node(int profundidad, Escenario* element, Node* padre, Node* left, Node* right);
+		int id; // 1: Nodo izquierdo, 2:Nodo derecho ---->
+		Node(int profundidad, Escenario* element, Node* padre, Node* left, Node* right , int id); //
 	};
 
 	Node* root;
 	Node* current;
+	Node* auxCurrent;
 	int puntos;
 
 public:
@@ -39,6 +43,56 @@ public:
 	void Damage();
 
 	void CheckProyectiles();
+
+	void save_inOrder_Tree(Node* node,XmlWriter^ writer) {
+
+			if (node != nullptr) {
+				save_inOrder_Tree(node->left_Child_Esc, writer);
+				save_Node(node, writer, node->profundidad, node->id);
+				save_inOrder_Tree(node->right_Child_Esc, writer);
+			}
+
+	}
+
+	void save_tree() {
+
+		XmlWriter^ writer = XmlWriter::Create("Archivo.xml");
+		writer->WriteStartElement("DATA");
+		save_inOrder_Tree(this->auxCurrent, writer);
+		writer->WriteEndElement();
+
+		writer->Close();
+
+		delete writer;
+
+
+	}
+
+
+
+	
+	void save_Node(Node* node, XmlWriter^ writer, int nivel, int id) {
+
+		writer->WriteStartElement("Node");
+		writer->WriteString(nivel.ToString() + id.ToString()); // nivel y id (1: node izq , 2: node der)
+		writer->WriteEndElement();
+
+		writer->WriteStartElement("Profundidad");
+		writer->WriteString(node->profundidad.ToString()); // Prof
+		writer->WriteEndElement();
+
+
+		writer->WriteStartElement("NShooter"); //<---------------------
+		writer->WriteString(node->element->n_Shooter().ToString());// <<<<-- Implementar una funcion que retorne el numero de Enemigos Shooters
+		writer->WriteEndElement();
+
+		writer->WriteStartElement("NMele");
+		writer->WriteString(node->element->n_Mele().ToString()); //<<<--Implementar una funcion que retorne el numero de Enemigos Melee
+		writer->WriteEndElement();
+		
+
+
+	}
 
 	Player* GetPLayer();
 };
